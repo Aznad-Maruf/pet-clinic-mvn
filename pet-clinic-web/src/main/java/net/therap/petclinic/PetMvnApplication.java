6e2c262587344b63a1d972047836f8c5128623b1
khandaker.maruf <khandaker.maruf@therapservices.net>
2022-07-06 15:07:38 +0600

    init

---
+++ pet-clinic-web/src/main/java/net/therap/petclinic/PetMvnApplication.java
@@ -0,0 +1,13 @@
+package net.therap.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetMvnApplication.class, args);
+	}
+
+}
---
+++ pet-clinic-web/src/test/java/net/therap/petclinic/PetMvnApplicationTest.java
@@ -0,0 +1,11 @@
+package net.therap.petclinic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author khandaker.maruf
+ * @since 7/6/22
+ */
+class PetMvnApplicationTest {
+
+}
